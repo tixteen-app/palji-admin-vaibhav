8317a7d5f88a63db3b247737ccb41ee0033d2f72
thevaibhavrr <vaibhavrathorema@gmail.com>
2025-04-24 11:54:55 +0530

    update clodudany id and pass

--- src/utils/cloudinaryUpload.js
+++ src/utils/cloudinaryUpload.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import imageCompression from 'browser-image-compression';
+import axios from "axios";
+import imageCompression from "browser-image-compression";
 
 const uploadToCloudinary = async (file, setUploadProgress, maxSizeKB = 800) => {
   try {
@@ -14,26 +14,32 @@ const uploadToCloudinary = async (file, setUploadProgress, maxSizeKB = 800) => {
 
     const data = new FormData();
     data.append("file", compressedFile);
-    data.append("upload_preset", "pfendx01");
-    data.append("folder", "Saburi");
+    // data.append("upload_preset", "pfendx01");
+    // data.append("folder", "Saburi");
+    data.append("upload_preset", "grzbbapu");
+    data.append("folder", "palji");
 
     const response = await axios.post(
-      `https://api.cloudinary.com/v1_1/dwxtuqnty/upload`,
+      // `https://api.cloudinary.com/v1_1/dwxtuqnty/upload`,
+					`https://api.cloudinary.com/v1_1/dh74mrqe0/upload`,
+
       data,
       {
         onUploadProgress: (progressEvent) => {
-          const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+          const percentCompleted = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
           if (setUploadProgress) {
             setUploadProgress(percentCompleted);
           }
-        }
+        },
       }
     );
 
     if (response.status === 200) {
       return response.data.url;
     } else {
-      throw new Error('Failed to upload image');
+      throw new Error("Failed to upload image");
     }
   } catch (error) {
     console.error("Image upload error:", error);
